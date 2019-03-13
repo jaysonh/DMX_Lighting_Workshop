51d11fba26a623892c6cc7e8f6829b76ad77bb94
jaysonh <jaysonkh@gmail.com>
2019-03-13 15:40:23 +0800

    kdkd

---
+++ day 3 code/WallWasher/WallWasher.ino
@@ -0,0 +1,26 @@
+#include <Conceptinetics.h>
+
+DMX_Master dmx_master ( 100 , 2 );
+
+int r = 64;
+void setup() {  
+    dmx_master.enable ();
+    for(int i =0 ; i < 100;i++)
+    {
+      dmx_master.setChannelValue ( i, 0); 
+    }
+}
+
+void loop() 
+{ 
+    // 9 channel mode
+    dmx_master.setChannelValue ( 0,  0);   // 
+    dmx_master.setChannelValue ( 1,  0);   // red
+    dmx_master.setChannelValue ( 2,  0);   // green
+    dmx_master.setChannelValue ( 3,255);   // blue
+    
+    dmx_master.setChannelValue ( 7, 255);   // dimmer
+    
+    
+    delay ( 100 );
+}
